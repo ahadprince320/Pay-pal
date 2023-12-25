d9e3ef52d4ef6311a89f3dadb6db8615cca3e2e9
prince-ahad <ahadprince320@gmail.com>
2023-12-25 14:37:11 -0800

    second commit

--- lib/navbar_screen/home_screen/Sent_maney/sent_money.dart
+++ lib/navbar_screen/home_screen/Sent_maney/sent_money.dart
@@ -24,47 +24,50 @@ class _SentMoneyState extends State<SentMoney> {
           )],
           context: context
       ),
-      body: Column(
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
 
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-                backgroundColor: Colors.grey,
-                child: Text('Prince Ahad'[0])),
-            title: Text('To:'),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
-                Text('Prince Ahad'),
-                Text('prince3020@gmail.com'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextFromField(hintText: 'What is your amount'),
-          ),
-          Container(
-            height: Size.height*.58,
-
-            child: Padding(
-              padding: const EdgeInsets.all(17.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CustromButtom(
-                    name: 'Sent',
-                    onPressed: () {
-                   Navigator.push(context, MaterialPageRoute(builder: (context) => SuccessPAy(),));
-                      print('sorry');
-                    },
-                  ),
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Text('Prince Ahad'[0])),
+              title: Text('To:'),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:[
+                  Text('Prince Ahad'),
+                  Text('prince3020@gmail.com'),
                 ],
               ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFromField(hintText: 'What is your amount'),
+            ),
+            Container(
+              height: Size.height*.58,
 
-        ],
+              child: Padding(
+                padding: const EdgeInsets.all(17.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CustromButtom(
+                      name: 'Sent',
+                      onPressed: () {
+                     Navigator.push(context, MaterialPageRoute(builder: (context) => SuccessPAy(),));
+                        print('sorry');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
